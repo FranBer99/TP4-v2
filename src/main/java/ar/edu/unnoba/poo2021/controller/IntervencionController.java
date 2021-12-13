a8daf28ae6d32d24846b9736611015df33de4f6e
Francisco Berruet <elfranchoberruet@gmail.com>
2021-12-13 12:16:23 -0300

    wip
    work in progress

--- src/main/java/ar/edu/unnoba/poo2021/controller/IntervencionController.java
+++ src/main/java/ar/edu/unnoba/poo2021/controller/IntervencionController.java
@@ -1,12 +1,12 @@
 package ar.edu.unnoba.poo2021.controller;
 
+import ar.edu.unnoba.poo2021.model.entity.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import ar.edu.unnoba.poo2021.model.entity.Intervencion;
@@ -38,6 +38,20 @@ public class IntervencionController {
             redirectAttributes.addFlashAttribute("error",e.getMessage());
             return "redirect:/intervenciones/reg_intervencion"; }
         return "redirect:/intervenciones/vista_intervenciones";
+    }
+
+    @GetMapping("/borrar/{id}")
+    public String userDelete(@PathVariable("id") Long interventionId,
+                             RedirectAttributes redirectAttributes){
+        intervencionService.delete(interventionId);
+        return "redirect:/intervenciones/vista_intervenciones";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String userEdit(@PathVariable("id") Long userId, Model model){
+        Intervencion intervencion  = intervencionService.findById(userId);
+        model.addAttribute("intervencion",intervencion);
+        return "intervenciones/editar";
     }
 
 }
--- src/main/java/ar/edu/unnoba/poo2021/model/repository/IntervencionRepository.java
+++ src/main/java/ar/edu/unnoba/poo2021/model/repository/IntervencionRepository.java
@@ -1,14 +1,7 @@
 package ar.edu.unnoba.poo2021.model.repository;
 
 import ar.edu.unnoba.poo2021.model.entity.Intervencion;
-
-import java.sql.Date;
-import java.time.LocalDateTime;
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
