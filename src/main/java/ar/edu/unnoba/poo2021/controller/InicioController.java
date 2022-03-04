dccfacd40d314cea3fc516f870267cf9478351ad
lucasfernandez866 <85810432+Lucasfernan152@users.noreply.github.com>
2022-03-04 12:28:43 -0300

    front user

--- src/main/java/ar/edu/unnoba/poo2021/controller/InicioController.java
+++ src/main/java/ar/edu/unnoba/poo2021/controller/InicioController.java
@@ -1,10 +1,18 @@
 package ar.edu.unnoba.poo2021.controller;
 
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class InicioController {
+
+    protected Optional<String> getPreviousPageByRequest(HttpServletRequest request) {
+        return Optional.ofNullable(request.getHeader("Referer")).map(requestUrl -> "redirect:" + requestUrl);
+    }
 
     @GetMapping("/")
     public String inicio(){
